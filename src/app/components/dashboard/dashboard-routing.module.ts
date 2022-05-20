a0b23b80545450de94b0d2d2974880edc362d22d
martinagorriaran <martina.gorriaraan@gmail.com>
2022-05-19 21:15:46 -0300

    dfgfd

--- src/app/components/dashboard/dashboard-routing.module.ts
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ReportesComponent } from './reportes/reportes.component';
+import { CrearUsuarioComponent } from './usuarios/crear-usuario/crear-usuario.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path:'',component: InicioComponent},
   {path:'usuarios',component: UsuariosComponent},
   {path:'reportes',component: ReportesComponent },
+  {path:'crear-usuario',component: CrearUsuarioComponent },
 ]}
 
 ];
--- src/app/components/dashboard/usuarios/usuarios.component.ts
+++ src/app/components/dashboard/usuarios/usuarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Usuario } from 'src/app/interfaces/usuario';
@@ -25,7 +26,7 @@ export class UsuariosComponent implements OnInit {
 
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private _usuarioService: UsuarioService) { }
+  constructor(private _usuarioService: UsuarioService,private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.cargarUsuarios();
@@ -33,7 +34,7 @@ export class UsuariosComponent implements OnInit {
 
   cargarUsuarios(){
     this.listUsuarios =  this._usuarioService.getUsuario();
-    this.dataSource = new MatTableDataSource(this.listUsuarios)
+    this.dataSource = new MatTableDataSource(this.listUsuarios);
   }
 
 
@@ -55,6 +56,12 @@ export class UsuariosComponent implements OnInit {
 
     this._usuarioService.eliminarUsuario(index);
     this.cargarUsuarios();
+
+    this._snackBar.open('Usuario fue eliminado con exito','',{
+      duration: 1500,
+      horizontalPosition:'center',
+      verticalPosition: 'bottom'
+    })
   }
 
   
