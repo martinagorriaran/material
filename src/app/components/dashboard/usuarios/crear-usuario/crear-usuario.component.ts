d9b2bf486f57b55261dba4a1aa90d029e9552ab8
martinagorriaran <martina.gorriaraan@gmail.com>
2022-05-20 10:03:57 -0300

    u

--- src/app/components/dashboard/usuarios/crear-usuario/crear-usuario.component.ts
+++ src/app/components/dashboard/usuarios/crear-usuario/crear-usuario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
@@ -12,7 +14,10 @@ export class CrearUsuarioComponent implements OnInit {
   sexo: any[] = ['Masculino','Femenino'];
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private _usuarioService: UsuarioService ) {
+  constructor(private fb: FormBuilder, 
+              private _usuarioService: UsuarioService,
+              private router: Router,
+              private _snackBar: MatSnackBar ) {
     this.form = this.fb.group({
       usuario:['',Validators.required],
       nombre:['',Validators.required],
@@ -35,6 +40,13 @@ export class CrearUsuarioComponent implements OnInit {
     }
 
     this._usuarioService.agregarUsuario(user);
+    this.router.navigate(['/dashboard/usuarios']);
+
+    this._snackBar.open('Usuario fue agregado con exito','',{
+      duration: 1500,
+      horizontalPosition:'center',
+      verticalPosition: 'bottom'
+    })
   }
 
 }
